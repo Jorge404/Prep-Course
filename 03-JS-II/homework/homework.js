83b947cdf7d369c6f0be75a1a7a79b092a6d5445
Jorge404 <jorgeluismarquezsanchez6@gmail.com>
2022-05-05 20:41:39 -0500

    prueba

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,19 +150,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
-  if (num1 > num2 && num1 > num3 && num1 > 0) {
-      return 'Numero 1 es mayor y positivo';
-  }else if(num1 < 0 || num2 < 0|| num3 < 0){
-      return 'Hay negativos';
+  if(num1 < 0 || num2 < 0|| num3 < 0){
+      return "Hay negativos";
+  }else if(num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Numero 1 es mayor y positivo";
   }else if(num3 > num1 && num3 > num2){
         num3++;
       return num3;
   }else if(num1 === 0 || num2 === 0 || num3 === 0){
-      return 'Error';
+      return "Error";
 
-  }else
+  }else{
       return false;
+}
 }
 
 function esPrimo(numero) {
@@ -171,12 +171,22 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
+  for( i = 0; i < numero; i++){
+    if(numero % i === 0){
+      return false;
+    }
+  }
+
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor === false){
+    return 'Soy verdadero'
+  }
 
 }
 
